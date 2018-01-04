2bde25464834ca9bbeb83b08ab4e307297630ce9
Niklas Larsson <niklaslarsson95@gmail.com>
2018-01-04 18:27:45 +1100

    Improve input system

--- src/main.rs
+++ src/main.rs
@@ -31,36 +31,52 @@ fn main() {
     deal(&mut game, &mut deck);
 
     let mut ch;
+    let mut src_pile;
+    let mut src_depth;
+    let mut dest_pile;
     loop {
-            term::clear();
+        term::clear();
         if game_won(&mut game) {
-            term::printw("============================================\nWIN\n============================================");
+            term::printw("=======================================\nWIN\n=======================================");
             game_restart(&mut game);
         }
         print_game(&game);
-        term::printw("Src pile?: ");
-        term::refresh();
-        ch = term::getch();
-        let src_pile = index_from_char(ch);
+        let mut valid = false;
+        while !valid {
+            loop {
+                term::printw("\nSrc pile?: ");
+                term::refresh();
+                ch = term::getch();
+                src_pile = src_index_from_char(ch);
+                if src_pile != 99 { break };
+            }
 
-        if src_pile == 12 {
-            draw(&mut game);
-        } else if src_pile >= 13 {
-            game_restart(&mut game);
-        } else {
+            if src_pile == 12 {
+                draw(&mut game);
+            } else if src_pile == 13 {
+                game_restart(&mut game);
+            } else {
 
-            term::printw("\nsrc depth?: ");
-            term::refresh();
-            ch = term::getch();
-            let src_depth = index_from_char(ch);
+                loop {
+                    term::printw("\nsrc depth?: ");
+                    term::refresh();
+                    ch = term::getch();
+                    src_depth = depth_from_char(ch);
+                    if src_depth != 99 { break };
+                }
 
-            term::printw("\ndest_pile?: ");
-            term::refresh();
-            ch = term::getch();
-            let dest_pile = index_from_char(ch);
+                loop {
+                    term::printw("\ndest_pile?: ");
+                    term::refresh();
+                    ch = term::getch();
+                    dest_pile = dest_index_from_char(ch);
+                    if dest_pile != 99 { break };
+                }
 
-            term::printw(&format!("\n{}, {}, {}\n", src_pile, src_depth, dest_pile));
-            make_move(&mut game, src_pile, src_depth, dest_pile);
+                term::printw(&format!("\n{}, {}, {}\n", src_pile, src_depth, dest_pile));
+                term::refresh();
+                valid = make_move(&mut game, src_pile, src_depth, dest_pile);
+            }
         }
     }
 
@@ -71,22 +87,50 @@ fn main() {
 // zero through 6 refer to the piles, h s d c refers to the foundations indices 7 through 10, 
 // q draws from the hand and w draws from the side deck to the hand
 // r restarts the game
-fn index_from_char(ch: i32) -> usize {
+fn src_index_from_char(ch: i32) -> usize {
     match char::from_u32(ch as u32).expect("Invalid char") {
-        '0' => 0,
+        '1' => 0,
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        'q' => 11,
+        'w' => 12,
+        'r' => 13,
+        _ => 99,
+    }
+}
+
+fn dest_index_from_char(ch: i32) -> usize {
+    match char::from_u32(ch as u32).expect("Invalid char") {
+        '1' => 0,
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        'h' => 7,
+        's' => 8,
+        'd' => 9,
+        'c' => 10,
+        _ => 99,
+    }
+}
+
+fn depth_from_char(ch: i32) -> usize {
+    match char::from_u32(ch as u32).expect("Invalid char") {
         '1' => 1,
         '2' => 2,
         '3' => 3,
         '4' => 4,
         '5' => 5,
         '6' => 6,
-        'h' => 7,
-        's' => 8,
-        'd' => 9,
-        'c' => 10,
-        'q' => 11,
-        'w' => 12,
-        'r' => 13,
-        _ => 99,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        _ =>  99,
     }
 }
